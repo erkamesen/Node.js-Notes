5b759f8a70eccbfbe3d7b2a6662c3e3211f4f2a2
erkamesen <bmth_789@hotmail.com>
2023-03-23 22:01:06 +0300

    Add files

---
+++ for_in_.js
@@ -0,0 +1,17 @@
+/*  - - - - - - - For in - - - - - - - - - */
+console.log("------------".repeat(3))
+
+
+var object = {
+    a: 1, b: 2, c: 3
+}
+
+for(var key in object){
+    console.log(key)
+}
+/* 
+a
+b
+c
+*/
+
